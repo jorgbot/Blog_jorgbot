862801ccab110c537d89e302f16122764f418af9
jorgbot <jbotelloangarita@hotmail.com>
2015-12-22 01:32:48 -0500

    Add edit, destroy proyecto habilitado

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -20,7 +20,22 @@ class ProjectsController < ApplicationController
 	end
 
 	def show
-		
+	end
+
+	def edit
+	end
+
+	def update
+		if @project.update project_params
+			redirect_to @project, notice: "Super Jorge Botello! Este proyecto se ha actualizado correctamente!"
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@project.destroy
+		redirect_to projects_path
 	end
 
 	private
